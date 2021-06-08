7aec0ab79c400a2a12090c7f2c41c0ddb18b162e
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-06-08 16:44:00 +0200

    Header Menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('tag_title')</title>
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,6 +16,58 @@
 
             </div>
         </div>
+
+        {{-- Header Nav --}}
+        <nav>
+            <div class="container">
+
+                {{-- Logo --}}
+                <div class="logo">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo">
+                </div>
+
+                {{-- Menu --}}
+                <ul class="menu">
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop <span>&#9660</span></a>
+                    </li>
+                </ul>
+
+                {{-- Search --}}
+                <div class="search">
+                    Search
+                </div>
+
+
+            </div>            
+        </nav>
     
     
 </header>
