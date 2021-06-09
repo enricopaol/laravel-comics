64a74c5886efdbf6bf51dde8732a3f106d28c89f
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-06-09 16:22:37 +0200

    Footer Top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,140 @@
 <footer>
-    ciao sono il footer
+
+    {{-- Footer Top Nav --}}
+    <div class="footer-top">
+        <div class="container">
+
+            {{-- Footer Nav Left --}}
+            <nav class="footer-nav">
+
+                {{-- Footer Nav Top --}}
+                <div class="footer-nav-top">
+
+                    {{-- Footer Nav Col --}}
+                    <div class="footer-nav-col">
+                        <h4 class="footer-nav-title">DC Comics</h4>
+                        <ul class="footer-nav-menu">
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                        </ul>
+
+                        <h4 class="footer-nav-title">Shop</h4>
+                        <ul class="footer-nav-menu">
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop DC Collections</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+
+                    {{-- Footer Nav Col --}}
+                    <div class="footer-nav-col">
+                        <h4 class="footer-nav-title">DC</h4>
+                        <ul class="footer-nav-menu">
+                            <li>
+                                <a href="#">Terms Of Use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy (New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {{-- Footer Nav Col --}}
+                    <div class="footer-nav-col">
+                        <h4 class="footer-nav-title">Sites</h4>
+                        <ul class="footer-nav-menu">
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>                        
+                        </ul>
+                    </div>
+
+                </div>
+
+                {{-- Footer Nav Copyright --}}
+                <div class="footer-nav-copyright">
+                    <p>
+                        All Site Content TM and c 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. 
+                        <br>
+                        <a href="#">Cookie Settings</a>
+                    </p>
+                </div>
+
+            </nav>
+
+            {{-- Footer Logo Right --}}
+            <div class="footer-logo">
+                <img src="{{ asset('img/dc-logo-bg.png') }}" alt="dc-logo">
+            </div>
+
+        </div>
+    </div>
+
+    {{-- Footer Bottom --}}
+    <div class="footer-bottom">
+
+    </div>
+
 </footer>
