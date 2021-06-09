12c2da543dedcb643655facb2366ad58706766c5
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-06-09 13:07:20 +0200

    End of main first page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,8 +32,42 @@
                 <a href="#" class="btn">Load More</a>
             </div>
             
+        </div>
+    </section>
 
+    {{-- CTA Section BG Blue --}}
+    <section class="cta-section-bg-blue">
+        <div class="container">
 
+            {{-- Single CTA --}}
+            <div class="cta-single">
+                <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="buy digital comics">
+                <h4 class="cta-title">Digital Comics</h4>
+            </div>
+
+            {{-- Single CTA --}}
+            <div class="cta-single">
+                <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="merchandise">
+                <h4 class="cta-title">DC Merchandise</h4>
+            </div>
+
+            {{-- Single CTA --}}
+            <div class="cta-single">
+                <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="subscription">
+                <h4 class="cta-title">Subscription</h4>
+            </div>
+
+            {{-- Single CTA --}}
+            <div class="cta-single">
+                <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="shop-locator">
+                <h4 class="cta-title">Comic Shop Locator</h4>
+            </div>
+
+            {{-- Single CTA --}}
+            <div class="cta-single">
+                <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="dc-power-visa">
+                <h4 class="cta-title">DC Power Visa</h4>
+            </div>
 
         </div>
     </section>
