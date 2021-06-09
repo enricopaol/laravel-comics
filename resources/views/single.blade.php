7364ff74f3cb15cf474d26c2e26dada94783a144
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-06-09 17:23:37 +0200

    Poster e title

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.app')
 
 @section('tag_title')
-    Comics Series: {{$comicInfo['title']}}
+    {{$comicInfo['title']}}
 @endsection
 
 @section('main_content')
     
+    {{-- Comic Poster Section --}}
+    <section class="comic-poster">
+        <div class="container">
+            <img src="{{ $comicInfo['thumb'] }}" alt="">
+        </div>        
+    </section>
+
+    {{-- Comic Presentation Section --}}
+    <section class="comic-presentation">
+        <div class="container">
+
+            {{-- Comic Info --}}
+            <div class="comic-info">
+                <h1>
+                    {{ $comicInfo['title'] }}
+                </h1>
+            </div>
+
+            {{-- Advertisement --}}
+            <div class="advertisement">
+
+            </div>
+
+        </div>
+    </section>
+
 @endsection
