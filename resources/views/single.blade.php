9f8fbbeae77c55f8f665ade16ddac302c7c8276f
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-06-09 17:03:23 +0200

    Creata route e passate info a seconda dell'id

---
+++ resources/views/single.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('tag_title')
+    Comics Series: {{$comicInfo['title']}}
+@endsection
+
+@section('main_content')
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,25 @@ Route::get('/', function () {
     return view('home', $data);
 })->name('home');
 
+Route::get('/single-comic/{id}', function ($id) {
+    $comics = config('comics');
+
+    $comicInfo = [];
+    foreach($comics as $comic) {
+        if($comic['id'] === (int) $id) {
+            $comicInfo = $comic;
+        }
+    }
+
+    if(empty($comicInfo)) {
+        abort('404');
+    }
+
+    $data = [
+        'comicInfo' => $comicInfo
+    ];    
+
+    return view('single', $data);
+})->name('single');
+
 
